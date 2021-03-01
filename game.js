1adda14562158fc094b919fde49bb75371c0d0ca
Seb Bains <sebastianbains@gmail.com>
2021-03-01 21:26:20 +0000

    added hud elements and scores

---
+++ game.js
@@ -0,0 +1,100 @@
+const question = document.querySelector('#question');
+const choices = [...document.querySelectorAll('.choice-text')];
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBar = document.querySelector('#progressBar');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+let questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript??',
+        choice1: '<script>',
+        choice2: '<javascript>',
+        choice3: '<js>',
+        choice4: '<scripting>',
+        answer: 1,
+    },
+    {
+        question:
+            "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script name='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<script file='xxx.js'>",
+        answer: 3,
+    },
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    },
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+const startGame = () => {
+    // reset
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+
+    getNewQuestion();
+}
+
+const getNewQuestion = () => {
+    // check if questions remain
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("/end.html");
+    }
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    // get question
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    // get choices
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion[`choice${number}`];
+    })
+    // update remaining questions
+    availableQuestions.splice(questionIndex,1);
+    acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+    // check answer
+    choice.addEventListener("click", (e) => {
+        if(!acceptingAnswers) return;
+        acceptingAnswers = false;
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset["number"];
+        const classToApply = selectedAnswer == currentQuestion.answer? 'correct' : 'incorrect';
+        if( classToApply === 'correct'){
+            incrementScore(CORRECT_BONUS);
+        }
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);  
+    })
+})
+
+const incrementScore = num => {
+    // updates score
+    score += num;
+    scoreText.innerText = score;
+}
+
+startGame();
